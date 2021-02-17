bba27cbdae2f418127ca85d16ce2b5c140eb4ad4
68994510+pkakria@users.noreply.github.com <68994510+pkakria@users.noreply.github.com>
2021-02-17 12:57:09 -0800

    updated main activity page

--- app/src/main/java/com/example/covidtimes/MainActivity.java
+++ app/src/main/java/com/example/covidtimes/MainActivity.java
@@ -4,24 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public void clk_login(View view){
+        Log.i("this", "this is a message");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       final Button infoButton = findViewById(R.id.btnInfo);
-       infoButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent infoIntent = new Intent(getApplicationContext(), infoActivity.class);
-               startActivity(infoIntent);
-           }
-       });
-
+        final Button clickbtn = findViewById(R.id.about_button);
+        clickbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent infoActivityIntent = new Intent(getApplicationContext(),infoActivity.class);
+                startActivity(infoActivityIntent);
+            }
+        });
+        final Button clkStat = findViewById(R.id.stat_button);
+        clkStat.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent statIntent = new Intent(getApplicationContext(), displayStats.class);
+                startActivity(statIntent);
+            }
+        });
+        final Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                clk_login(v);
+            }
+        });
     }
+
 }
